3e7b92e8b5cb326e65fc695cee41595c44815edc
Steven.Mitchell <Steven.Mitchell29@gmail.com>
2017-02-23 12:25:17 -0500

    added all the new stuff provided and fixed the links

--- template-minimal/js/main.js
+++ template-minimal/js/main.js
@@ -1,62 +1,49 @@
-window.onload = function() {
+"use strict";
 
-    
-    "use strict";
+window.onload = function() {
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        //load the images for the sprites
-        game.load.image('earth', 'assets/earth.png');
-        game.load.image('asteroid', 'assets/asteroid.png');
+        // Load an image and call it 'logo'.
+        game.load.image( 'logo', 'assets/phaser.png' );
     }
     
-    var score = 0;
-    var scoreText;
-
-    var rocks;
-    var earthy;
+    var bouncy;
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        earthy = game.add.sprite(0, 0, 'earth');
-        earthy.scale.setTo(.45, .33);
-   
-        //make a group of stars
-        rocks = game.add.group();
-
-        //  Here we'll create 15 in random positions
-        for (var i = 0; i < 15; i++) {
-            //  Create rocks inside of the rocks group
-            var x,y;
-            x = game.rnd.integerInRange(0,760);
-            y = game.rnd.integerInRange(0, 560);
-
-            //make it so that it is scaled and touchable
-            var rock = rocks.create(x, y, 'asteroid');
-            rock.scale.setTo(.1, .1);
-            rock.inputEnabled = true;
-            rock.input.useHandCursor = true;
-            rock.events.onInputDown.add(listener, this);
-        }
-
-        //  The score
-        scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: 'white' });
+        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        // Anchor the sprite at its center, as opposed to its top-left corner.
+        // so it will be truly centered.
+        bouncy.anchor.setTo( 0.5, 0.5 );
+        
+        // Turn on the arcade physics engine for this sprite.
+        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
+        // Make it bounce off of the world bounds.
+        bouncy.body.collideWorldBounds = true;
+        
+        // Add some text using a CSS style.
+        // Center it in X, and position its top 15 pixels from the top of the world.
+        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        text.anchor.setTo( 0.5, 0.0 );
     }
     
-    //do something with this later
     function update() {
-    }
-
-    function listener(rock, pointer) {
-        // Removes the rock from the screen
-        rock.kill();
-
-        //  Add and update the score
-        score += 1;
-        scoreText.text = 'Score: ' + score;
-        if (score === 15) {//if you clicked them all you win!
-            scoreText.text = 'YOU WIN!';
-        }
+        // Accelerate the 'logo' sprite towards the cursor,
+        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
+        // in X or Y.
+        // This function returns the rotation angle that makes it visually match its
+        // new trajectory.
+        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
     }
 };
