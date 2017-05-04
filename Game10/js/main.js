28c6f844db48b7e18beee2831dabf06e824b4e95
Steven.Mitchell <Steven.Mitchell29@gmail.com>
2017-05-03 20:18:33 -0400

    fgh

--- Game10/js/main.js
+++ Game10/js/main.js
@@ -14,6 +14,8 @@ window.onload = function () {
         //additions
         game.load.image('enemyBullet', 'assets/enemy-bullet.png');
         game.load.spritesheet('invader', 'assets/invader32x32x4.png', 32, 32);
+        game.load.audio('music', 'assets/SpaceTrip.mp3');
+
     }
 
     var player1;
@@ -37,11 +39,19 @@ window.onload = function () {
 
     //additions
     var aliens;
-    var powerup;
+    var powerups = 0;
+    var timer;
+    var total = 0;
+    var stateText;
+    var music;
 
 
 
     function create() {
+        music = game.add.audio('music');
+        //music.loop(5);
+        //music.play();
+        music.loopFull(.8);
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -52,18 +62,18 @@ window.onload = function () {
         player1bullets = game.add.group();
         player1bullets.enableBody = true;
         player1bullets.physicsBodyType = Phaser.Physics.ARCADE;
-        player1bullets.createMultiple(6, 'bullet');
+        player1bullets.createMultiple(3, 'bullet');
         player1bullets.setAll('anchor.x', 0.5);
         player1bullets.setAll('anchor.y', 1);
         player1bullets.setAll('outOfBoundsKill', true);
         player1bullets.setAll('checkWorldBounds', true);
-        
+
 
         // Player 2's bullets
         player2Bullet = game.add.group();
         player2Bullet.enableBody = true;
         player2Bullet.physicsBodyType = Phaser.Physics.ARCADE;
-        player2Bullet.createMultiple(6, 'bullet2');
+        player2Bullet.createMultiple(5, 'bullet2');
         player2Bullet.setAll('anchor.x', 0.5);
         player2Bullet.setAll('anchor.y', 1);
         player2Bullet.setAll('outOfBoundsKill', true);
@@ -73,7 +83,7 @@ window.onload = function () {
         enemyBullets = game.add.group();
         enemyBullets.enableBody = true;
         enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
-        enemyBullets.createMultiple(30, 'enemyBullet');
+        enemyBullets.createMultiple(5, 'enemyBullet');
         enemyBullets.setAll('anchor.x', 0.5);
         enemyBullets.setAll('anchor.y', 1);
         enemyBullets.setAll('outOfBoundsKill', true);
@@ -85,7 +95,7 @@ window.onload = function () {
 
         createAliens();
         //
-        
+
         //player 1
         player1 = game.add.sprite(50, 100, 'ship');
         player1.anchor.setTo(0.5, 0.5);
@@ -104,6 +114,10 @@ window.onload = function () {
         player2Lives = game.add.group();
         game.add.text(game.world.width - 250, 10, 'Player 2 Lives: ', { font: '34px Arial', fill: '#fff' });
 
+        stateText = game.add.text(game.world.centerX, game.world.centerY, ' ', { font: '84px Arial', fill: '#fff' });
+        stateText.anchor.setTo(0.5, 0.5);
+        stateText.visible = false;
+
         //setting up player 1 lives visual
         for (var i = 0; i < 3; i++) {
             var ship = player1lives.create(30 + (30 * i), 60, 'ship');
@@ -114,7 +128,7 @@ window.onload = function () {
 
         //setting up player 2 lives visual
         for (var i = 0; i < 3; i++) {
-            var ship2 = player2Lives.create(game.world.width - 100 + (30 * i), 60, 'ship2');
+            var ship2 = player2Lives.create(game.world.width - 30 - (30 * i), 60, 'ship2');
             ship2.anchor.setTo(0.5, 0.5);
             ship2.angle = 90;
             ship2.alpha = 0.4;
@@ -125,6 +139,7 @@ window.onload = function () {
         explosions.createMultiple(30, 'kaboom');
         explosions.forEach(setupPlayer1, this);
         explosions.forEach(setupPlayer2, this);
+        explosions.forEach(setupInvader, this);
 
         //setting up movement and fire for player 1
         player1up = game.input.keyboard.addKey(Phaser.Keyboard.W);
@@ -134,6 +149,21 @@ window.onload = function () {
         //setting up movement and fire for player 2
         player2Movement = game.input.keyboard.createCursorKeys();
         fireButton2 = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+
+        timer = game.time.create(false);
+
+        //  Set a TimerEvent to occur after 2 seconds
+        timer.loop(12000, updateCounter, this);
+
+        //  Start the timer running - this is important!
+        //  It won't start automatically, allowing you to hook it to button events and the like.
+        timer.start();
+    }
+    function updateCounter() {
+
+        total++;
+
     }
 
     //next two methods for explosion set up
@@ -147,6 +177,13 @@ window.onload = function () {
         player2.anchor.x = 0.5;
         player2.anchor.y = 0.5;
         player2.animations.add('kaboom');
+    }
+    function setupInvader(invader) {
+
+        invader.anchor.x = 0.5;
+        invader.anchor.y = 0.5;
+        invader.animations.add('kaboom');
+
     }
 
     //for movement of the ships and collision detection
@@ -184,10 +221,20 @@ window.onload = function () {
             game.physics.arcade.overlap(player1bullets, player2, player1HitsPlayer2, null, this);
         }
 
+        game.physics.arcade.overlap(player1bullets, aliens, player1HitsAlien, null, this);
+        game.physics.arcade.overlap(player2Bullet, aliens, player2HitsAlien, null, this);
+
+        if (timer.duration.toFixed(0) < 5) {
+            //alert();
+            createAliens();
+        }
+
+        //music.loop(2);
     }
 
     function render() {
-        //if i wanted to check things
+        //game.debug.text('Time until event: ' + timer.duration.toFixed(0), 32, 32);
+        //game.debug.text('Loop Count: ' + total, 32, 64);
     }
 
     function player1HitsPlayer2(player2, bullet) {
@@ -209,8 +256,15 @@ window.onload = function () {
         if (player2Lives.countLiving() < 1) {
             player2.kill();
             player2Bullet.callAll('kill');
+
+            stateText.text = " PLAYER 1 WINS! \n Click to restart";
+            stateText.visible = true;
+
+            //the "click to restart" handler
+            game.input.onTap.addOnce(restart, this);
         }
     }
+
     function player2HitsPlayer1(player1, player2Bullet) {
         player2Bullet.kill();
 
@@ -228,6 +282,12 @@ window.onload = function () {
         if (player1lives.countLiving() < 1) {
             player1.kill();
             player1bullets.callAll('kill');
+
+            stateText.text = " PLAYER 2 WINS! \n Click to restart";
+            stateText.visible = true;
+
+            //the "click to restart" handler
+            game.input.onTap.addOnce(restart, this);
         }
     }
 
@@ -277,13 +337,70 @@ window.onload = function () {
     //added functions
     //need to add a person at a random height!
     function createAliens() {
-                var alien = aliens.create(48, 50, 'invader');
-                alien.anchor.setTo(0.5, 0.5);
-                alien.animations.add('fly', [0, 1, 2, 3], 20, true);
-                alien.play('fly');
-                alien.body.moves = false;
+        var alien = aliens.create(48, 50, 'invader');
+        alien.anchor.setTo(0.5, 0.5);
+        alien.animations.add('fly', [0, 1, 2, 3], 20, true);
+        alien.play('fly');
+        alien.body.moves = false;
 
-                aliens.x = (game.world.width / 2)-50;
-                aliens.y = (game.world.height / 2)-50;
+        aliens.x = (game.world.width / 2) - 50;
+        aliens.y = 0;
+
+        var tween = game.add.tween(aliens).to({ y: game.world.height - 75 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);//.to({ y: 50 }, 1500, Phaser.Easing.Linear.None).loop();//, 0, 1000, true);
+        tween.onLoop.add(descend, this);
     }
+    function descend() {
+
+        aliens.y += 10;
+
+    }
+    function player1HitsAlien(player1bullets, alien) {
+
+        player1bullets.kill();
+        alien.kill();
+
+        var explosion = explosions.getFirstExists(false);
+        explosion.reset(alien.body.x, alien.body.y);
+        explosion.play('kaboom', 30, false, true);
+
+        game.rnd.integerInRange(0, 1);
+
+        var ship = player1lives.create(30 + (30 * player1lives.countLiving()), 60, 'ship');
+        ship.anchor.setTo(0.5, 0.5);
+        ship.angle = 90;
+        ship.alpha = 0.4;
+    }
+    function player2HitsAlien(player2Bullet, alien) {
+
+        player2Bullet.kill();
+        alien.kill();
+
+        var explosion = explosions.getFirstExists(false);
+        explosion.reset(alien.body.x, alien.body.y);
+        explosion.play('kaboom', 30, false, true);
+
+        game.rnd.integerInRange(0, 1);
+
+        var ship2 = player2Lives.create(game.world.width - 30 - (30 * player2Lives.countLiving()), 60, 'ship2');
+        ship2.anchor.setTo(0.5, 0.5);
+        ship2.angle = 90;
+        ship2.alpha = 0.4;
+    }
+
+    function restart() {
+        //resets the life count
+        player1lives.callAll('revive');
+        player2Lives.callAll('revive');
+
+        aliens.removeAll();
+        createAliens();
+
+        player1.revive();
+        player2.revive();
+
+        //hides the text
+        stateText.visible = false;
+    }
+
+
 };
